1abf7d0e751a0d7ac3b0b12725ea86aa245099d9
Yuanjie Li <yuanjieli@MacBook-Air.local>
2026-06-09 15:59:51 -0600

    [INIT] project

---
+++ MoistureSensorHub/MoistureSensorHub.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  // Connect to WiFi
+
+  // Ping RasPi to make sure she's up
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
